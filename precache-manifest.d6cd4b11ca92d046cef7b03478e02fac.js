fc20d7a9431a7f6b098ad64f10ac8c04de242ef0
deepak9013216128 <deepak9013216128@gmail.com>
2020-07-29 21:31:03 +0530

    Updates

---
+++ precache-manifest.d6cd4b11ca92d046cef7b03478e02fac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85729206af6075b31dee447ed315925d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "110d720d143e7a5d4798",
+    "url": "/portfolio/static/css/main.1fa33b75.chunk.css"
+  },
+  {
+    "revision": "1437bc6d514a224753a0",
+    "url": "/portfolio/static/js/2.ba0a1b24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ba0a1b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110d720d143e7a5d4798",
+    "url": "/portfolio/static/js/main.98664054.chunk.js"
+  },
+  {
+    "revision": "b93ef1927cca321238ca",
+    "url": "/portfolio/static/js/runtime-main.aa010ad9.js"
+  },
+  {
+    "revision": "c176cf142db9264b21d3f3ac142520c2",
+    "url": "/portfolio/static/media/deepak_kumar.c176cf14.jpg"
+  },
+  {
+    "revision": "f07aafe569bb615b11f6b902a186da40",
+    "url": "/portfolio/static/media/img-01.f07aafe5.jpg"
+  },
+  {
+    "revision": "7ac1058d15e9b3b55b58e4b1b726913c",
+    "url": "/portfolio/static/media/img-02.7ac1058d.png"
+  },
+  {
+    "revision": "3579e6191c9cd3a80a8daacf287e1505",
+    "url": "/portfolio/static/media/img-03.3579e619.png"
+  },
+  {
+    "revision": "cf5ba9c2d618c46a81545301d0755a80",
+    "url": "/portfolio/static/media/img-04.cf5ba9c2.png"
+  }
+]);
